808b7add3f3fdfe9e142f57ad3b24fbd32c6f2d5
Jieliang Xu <tkzs2000@gmail.com>
2025-05-26 16:53:55 -0400

    Create one test for Equilateral triangle

---
+++ TestClass/TriangleTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TestClass
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void TestEquilateralTriangle()
+        {
+            // Arrange
+            int sideA = 5;
+            int sideB = 5;
+            int sideC = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("Equilateral triangle", result);
+        }
+    }
+}
